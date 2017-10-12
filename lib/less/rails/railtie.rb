5c6304c9b7412f99f4cca1b03b21adf3c96af094
Brendon Muir <brendon@spike.net.nz>
2017-10-12 22:20:03 +1300

    Revert mime type change and try adding DirectiveProcessor

--- lib/less/rails/railtie.rb
+++ lib/less/rails/railtie.rb
@@ -20,7 +20,8 @@ module Less
         end
 
         if sprockets_env.respond_to?(:register_transformer)
-          sprockets_env.register_mime_type 'text/css', extensions: ['.less', '.less.erb', '.less.css'], charset: :css
+          sprockets_env.register_mime_type 'text/less', extensions: ['.less', '.less.erb', '.less.css'], charset: :css
+          sprockets_env.register_preprocessor 'text/less', DirectiveProcessor.new(comments: ["//", ["/*", "*/"]])
           sprockets_env.register_transformer 'text/less', 'text/css', Grease.apply(LessTemplate)
         end
       end
